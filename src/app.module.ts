d51c7989d90d9d151d451ea6633a4838c8733202
antish@finestardiamonds.com <antish@finestardiamonds.com>
2024-08-12 14:31:14 +0530

    city add

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { CityModule } from './city/city.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CityModule],
   controllers: [AppController],
   providers: [AppService],
 })
