352b6d54b0a9bd5c511a0c6715ba76d775107d6a
antish@finestardiamonds.com <antish@finestardiamonds.com>
2024-08-13 15:02:06 +0530

    TEST

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CityModule } from './city/city.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [AuthModule, CityModule],
+  imports: [
+     AuthModule,
+     CityModule,
+     ConfigModule.forRoot({
+      envFilePath:'.env'
+     })
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
